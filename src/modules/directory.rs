b975ee2b68dc1d5d9c54a2d7a9592691f08df9b5
Christian Göttsche <cgzones@googlemail.com>
2026-02-19 13:29:46 +0100

    directory: reorder substitution

--- src/modules/directory.rs
+++ src/modules/directory.rs
@@ -67,21 +67,24 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
 
     let mut is_truncated = dir_string.is_some();
 
+    // Apply path substitutions
+    let display_dir: Cow<Path> = match substitute_path(
+        display_dir.to_string_lossy().into_owned(),
+        &config.substitutions,
+    ) {
+        Ok(result) => Cow::Owned(PathBuf::from(result)),
+        Err(err) => {
+            log::warn!("Invalid regex in directory substitutions: {err}");
+            Cow::Borrowed(display_dir)
+        }
+    };
+
     // the home directory if required.
     let dir_string = dir_string
-        .unwrap_or_else(|| contract_path(display_dir, &home_dir, config.home_symbol).to_string());
+        .unwrap_or_else(|| contract_path(&display_dir, &home_dir, config.home_symbol).to_string());
 
     #[cfg(windows)]
     let dir_string = remove_extended_path_prefix(dir_string);
-
-    // Apply path substitutions
-    let dir_string = match substitute_path(dir_string.clone(), &config.substitutions) {
-        Ok(result) => result,
-        Err(err) => {
-            log::warn!("Invalid regex in directory substitutions: {err}");
-            dir_string
-        }
-    };
 
     // Truncate the dir string to the maximum number of path components
     let dir_string =
@@ -97,7 +100,7 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
         // fish-style path contraction together
         if config.fish_style_pwd_dir_length > 0 && config.substitutions_empty() {
             // If user is using fish style path, we need to add the segment first
-            let contracted_home_dir = contract_path(display_dir, &home_dir, config.home_symbol);
+            let contracted_home_dir = contract_path(&display_dir, &home_dir, config.home_symbol);
             to_fish_style(
                 config.fish_style_pwd_dir_length as usize,
                 &contracted_home_dir,
@@ -112,7 +115,7 @@ pub fn module<'a>(context: &'a Context) -> Option<Module<'a>> {
 
     let path_vec = match &repo.and_then(|r| r.workdir.as_ref()) {
         Some(repo_root) if config.repo_root_style.is_some() => {
-            let contracted_path = contract_repo_path(display_dir, repo_root)?;
+            let contracted_path = contract_repo_path(&display_dir, repo_root)?;
             let repo_path_vec: Vec<&str> = contracted_path.split('/').collect();
             let after_repo_root = contracted_path.replacen(repo_path_vec[0], "", 1);
             let num_segments_after_root = after_repo_root.split('/').count();
@@ -1905,5 +1908,20 @@ mod tests {
             before_root_dir("~/user/gitrepo-diff/gitrepo", "aaa"),
             "~/user/gitrepo-diff/gitrepo".to_string()
         );
+    }
+
+    #[test]
+    fn home_symlink_substitution() {
+        let actual = ModuleRenderer::new("directory")
+            .env("HOME", "/home/username")
+            .path("/var/home/username/foo")
+            .config(toml::toml! {
+                [directory.substitutions]
+                "/var/home" = "/home"
+            })
+            .collect();
+        let expected = Some(format!("{} ", Color::Cyan.bold().paint("~/foo")));
+
+        assert_eq!(expected, actual);
     }
 }
